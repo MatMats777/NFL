561cd80ade8dd91fa169ffd8d4d036866023bc7d
Oliveira <315430@mil>
2015-11-15 19:09:42 -0500

    initial commit

---
+++ app/src/main/java/com/example/x61224/nfl/RankingActivityFragment.java
@@ -0,0 +1,22 @@
+package com.example.x61224.nfl;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class RankingActivityFragment extends Fragment {
+
+    public RankingActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_ranking, container, false);
+    }
+}
