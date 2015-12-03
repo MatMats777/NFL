50f0b121a9b296d5f241dfea508fc7915f0a3b75
Magnani <315429@mil>
2015-12-03 00:44:42 -0500

    Add the new empty classes

---
+++ app/src/main/java/com/example/x61224/nfl/RosterFragment.java
@@ -0,0 +1,7 @@
+package com.example.x61224.nfl;
+
+/**
+ * Created by x61221 on 12/3/2015.
+ */
+public class RosterFragment {
+}
---
+++ app/src/main/java/com/example/x61224/nfl/sync/AsyncTask.java
@@ -0,0 +1,7 @@
+package com.example.x61224.nfl.sync;
+
+/**
+ * Created by x61221 on 12/3/2015.
+ */
+public class AsyncTask {
+}
