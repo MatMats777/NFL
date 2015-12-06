145ee39bc9a3c9a0c4b2a3a7bf0a37ccca1e5b10
Magnani <315429@mil>
2015-12-06 11:16:30 -0500

    Re-edited the empty classes, working on them right now

--- app/src/main/java/com/example/x61224/nfl/RosterActivity.java
+++ app/src/main/java/com/example/x61224/nfl/RosterActivity.java
@@ -1,7 +1,29 @@
 package com.example.x61224.nfl;
 
-/**
- * Created by x61221 on 12/3/2015.
- */
-public class RosterActivity {
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class RosterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_roster);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
 }
--- app/src/main/java/com/example/x61224/nfl/RosterFragment.java
+++
@@ -1,7 +0,0 @@
-package com.example.x61224.nfl;
-
-/**
- * Created by x61221 on 12/3/2015.
- */
-public class RosterFragment {
-}
